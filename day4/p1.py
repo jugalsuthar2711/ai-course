54b3df8c8d24bbd21402d8789fd6c816fd18c0bf
jugalsuthar2711 <jugalsuthar2711@gmail.com>
2026-01-25 03:19:49 +0000

    lpwh

--- day4/p1.py
+++ day4/p1.py
@@ -1,1 +1,15 @@
-print("hello world....!!!")
+# for
+# while
+
+for i in range(0,20+1,5):
+    print(i, end=" ") 
+
+print("")
+num = 1
+
+while True:
+    if num>10:
+        break
+
+    print(num, end=" ")
+    num += 1
