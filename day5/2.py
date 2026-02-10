dfc00924c9fedee5f563751337233fe41b792a90
jugalsuthar2711 <jugalsuthar2711@gmail.com>
2026-02-10 15:37:23 +0000

    day5 file

---
+++ day5/2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+temp = np.array([1,3,4,5,6,3,2])
+
+# mean
+print(np.mean(temp))
+
+# meadin
+print(np.median(temp))
+
+[10,11,12]
+
+[10000, 12000, 15000, 70000]
+
+arr = np.array([19,33,66,32,76, np.nan, 44, np.nan])
+print(np.mean)
+print(np.median)
+print(np.std)
